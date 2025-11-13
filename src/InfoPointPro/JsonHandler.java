54613f0fe1550e6b923edede6202bae1a54dfa79
David <david_ofernandez@alumnos-santagema.es>
2025-11-13 19:33:23 +0100

    Json

--- src/InfoPointPro/JsonHandler.java
+++ src/InfoPointPro/JsonHandler.java
@@ -1,6 +1,43 @@
 package InfoPointPro;
 
+import java.io.File;
+
+import tools.jackson.databind.ObjectMapper;
+
 public class JsonHandler
 {
+	private static final String ACCOUNTJSONFILENAME = "accounts.json";
 	
+	private static File accountJSON = new File(ACCOUNTJSONFILENAME);
+	
+	
+	public JsonHandler()
+	{
+		
+	}
+	
+	
+	private static void main()
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Test test1 = new Test(0, "Hola");
+		Test test2 = new Test(12, "Adios");
+		
+		mapper.writeValue(accountJSON, test1);
+		
+	}
+	
+	
+	public class Test
+	{
+		int num;
+		String text;
+		
+		public Test(int num, String text)
+		{
+			this.num = num;
+			this.text = text;
+		}
+	}
 }
